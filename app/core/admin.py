245d177d4ae19f189d7b9eb53a79aa020146666d
itsjayeshp <jayeshpatidar1310@gmail.com>
2026-01-19 00:00:05 +0530

    feat: Implement User model administration in Django admin with custom list display and add tests.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """Manage user in the admin site."""
+    list_display = ["email", "name", "is_staff"]
+    ordering = ["email"]
+
+admin.site.register(models.User, UserAdmin)
+
+
+
