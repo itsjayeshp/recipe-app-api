7b74185959a747c7620f3447d66967dbc73e9ab3
itsjayeshp <jayeshpatidar1310@gmail.com>
2026-01-18 23:37:50 +0530

    feat: add tests for user model creation, email normalization, and superuser functionality

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -29,6 +29,20 @@ class ModelTests(TestCase):
             ("TEST3@Example.com", "TEST3@example.com"),
             ("test4@example.COM", "test4@example.com"),
         ]
-        for email , expected in sample_email:   
+        for email , expected in sample_email:
             user = get_user_model().objects.create_user(email, "testpass123")
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test creating user without email raises ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "testpass123")
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com", "testpass123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
