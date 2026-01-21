cd29cf195e2a4cae1ac599ea9fb06076beb6dfea
itsjayeshp <jayeshpatidar1310@gmail.com>
2026-01-21 22:58:19 +0530

    feat: Implement user update functionality and manage authenticated user view

--- app/user/views.py
+++ app/user/views.py
@@ -4,7 +4,7 @@ Views for the user API.
 
 # reminder all model , admin and migration will be handle in core app
 
-from rest_framework import generics
+from rest_framework import generics , authentication , permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import UserSerializer , AuthTokenSerializer
@@ -19,6 +19,15 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """ Manage the authenticated user """
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """ Retrieve and return the authenticated user. """
+        return self.request.user
 
 
 
