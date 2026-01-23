b932e5384eb8eacd2552530dea946ee7750e70fa
itsjayeshp <jayeshpatidar1310@gmail.com>
2026-01-23 23:09:06 +0530

    feat: Implement perform_create method to associate recipe with authenticated user

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -27,3 +27,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return serializers.RecipeSerializer
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create and return a recipe."""
+        serializer.save(user=self.request.user) 
