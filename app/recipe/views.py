6450e42a4c3bb6be55123b36cd5b2d61bd7831d0
itsjayeshp <jayeshpatidar1310@gmail.com>
2026-01-24 22:53:31 +0530

    feat: Add TagViewSet for managing tags in recipe API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,10 +2,10 @@
 Views for recipe api
 """
 
-from rest_framework import viewsets
+from rest_framework import viewsets , mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from core.models import Recipe
+from core.models import Recipe , Tag
 from recipe import serializers
 
 
@@ -30,3 +30,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create and return a recipe."""
         serializer.save(user=self.request.user)
+
+class TagViewSet(mixins.ListModelMixin , viewsets.GenericViewSet):
+    """View for managing tag api"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Return queryset for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+    
